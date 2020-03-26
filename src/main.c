f72c5f3c9795f789355623b73e10bb2514d9b22f
Alex van Vliet <alex.van-vliet@epita.fr>
2020-03-26 20:42:11 +0100

    feat: compute communities with louvain

--- src/main.c
+++ src/main.c
@@ -40,8 +40,39 @@ bool parse_options(options_t* options, int argc, char** argv)
     return true;
 }
 
+void compute_communities_louvain(igraph_t* graph)
+{
+    fprintf(stderr, "Running Louvain\n");
+
+    igraph_integer_t vcount = igraph_vcount(graph);
+
+    // Initialize communities
+    igraph_vector_t membership;
+    igraph_vector_init(&membership, vcount);
+
+    // Compute the communities
+    igraph_community_multilevel(graph, NULL, &membership,
+        NULL, NULL);
+
+    fprintf(stderr, "Clusters: %d\n",
+        (int) igraph_vector_max(&membership) + 1);
+
+    igraph_real_t modularity;
+    igraph_modularity(graph, &membership, &modularity, NULL);
+    fprintf(stderr, "Modularity: %f\n", modularity);
+
+    printf("Membership: ");
+    igraph_vector_print(&membership);
+    printf("\n");
+
+    // Destroy the communities
+    igraph_vector_destroy(&membership);
+}
+
 void compute_communities_leiden(igraph_t* graph)
 {
+    fprintf(stderr, "Running Leiden\n");
+
     igraph_integer_t vcount = igraph_vcount(graph);
     igraph_integer_t ecount = igraph_ecount(graph);
 
@@ -72,6 +103,10 @@ void compute_communities_leiden(igraph_t* graph)
     else
         fprintf(stderr, "Quality: %f\n", quality);
 
+    igraph_real_t modularity;
+    igraph_modularity(graph, &membership, &modularity, NULL);
+    fprintf(stderr, "Modularity: %f\n", modularity);
+
     printf("Membership: ");
     igraph_vector_print(&membership);
     printf("\n");
@@ -97,9 +132,12 @@ int main(int argc, char** argv)
     graph_information(options.input_name, &graph);
 
     // Write it as dot format on stdout
-    igraph_write_graph_dot(&graph, stdout);;
+    igraph_write_graph_dot(&graph, stdout);
 
-    // Compute the communities
+    // Compute the communities using louvain
+    compute_communities_louvain(&graph);
+
+    // Compute the communities using leiden
     compute_communities_leiden(&graph);
 
     // Destroy the graph
