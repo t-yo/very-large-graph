9c10d5ce65118a8bed0f66b5d509c7707abdf76c
Axen Georget <mail@axen.io>
2020-05-27 17:05:14 +0200

    feat: benchmark: add few information to display

--- src/benchmark.h
+++ src/benchmark.h
@@ -5,58 +5,59 @@
 
 #include <igraph.h>
 
-#define BENCHMARK(path, function)                                          \
-    do {                                                                   \
-        double global_elapsed = 0;                                         \
-        double total_elapsed = 0;                                          \
-        double total_diameter = 0;                                         \
-                                                                           \
-        struct timespec global_start;                                      \
-        clock_gettime(CLOCK_REALTIME, &global_start);                      \
-        double global_start_double =                                       \
-            global_start.tv_nsec * 1E-9 + global_start.tv_sec;             \
-                                                                           \
-        int tries = 0;                                                     \
-        FILE* file = fopen((path), "r");                                   \
-        igraph_t graph;                                                    \
-        igraph_read_graph_edgelist(&graph, file, 0, false);                \
-        fclose(file);                                                      \
-                                                                           \
-        while ((tries < 3) || (global_elapsed < 60))                       \
-        {                                                                  \
-            struct timespec start;                                         \
-            clock_gettime(CLOCK_REALTIME, &start);                         \
-                                                                           \
-            igraph_integer_t diameter = function(&graph);                  \
-                                                                           \
-            struct timespec end;                                           \
-            clock_gettime(CLOCK_REALTIME, &end);                           \
-                                                                           \
-                                                                           \
-            total_diameter += diameter;                                    \
-                                                                           \
-            double start_double = start.tv_nsec * 1E-9 + start.tv_sec;     \
-            double end_double = end.tv_nsec * 1E-9 + end.tv_sec;           \
-                                                                           \
-            total_elapsed += end_double - start_double;                    \
-                                                                           \
-            struct timespec current_time;                                  \
-            clock_gettime(CLOCK_REALTIME, &current_time);                  \
-            double current_time_double =                                   \
-                current_time.tv_nsec * 1E-9 + current_time.tv_sec;         \
-                                                                           \
-            global_elapsed = current_time_double - global_start_double;    \
-                                                                           \
-            tries += 1;                                                    \
-        }                                                                  \
-                                                                           \
-        igraph_destroy(&graph);                                            \
-                                                                           \
-        printf("Function: %s\n", #function);                               \
-        printf("Tries: %d\n", tries);                                      \
-        printf("Total elapsed: %f s\n", total_elapsed);                    \
-        printf("Average elapsed: %f s\n", total_elapsed / tries);          \
-        printf("Average time per try: %f try/s\n", tries / total_elapsed); \
-        printf("Average diameter: %f\n", total_diameter / tries);          \
-        printf("\n");                                                      \
+#define BENCHMARK(path, function)                                              \
+    do {                                                                       \
+        double global_elapsed = 0;                                             \
+        double total_elapsed = 0;                                              \
+        double total_diameter = 0;                                             \
+                                                                               \
+        struct timespec global_start;                                          \
+        clock_gettime(CLOCK_REALTIME, &global_start);                          \
+        double global_start_double =                                           \
+            global_start.tv_nsec * 1E-9 + global_start.tv_sec;                 \
+                                                                               \
+        int tries = 0;                                                         \
+        FILE* file = fopen((path), "r");                                       \
+        igraph_t graph;                                                        \
+        igraph_read_graph_edgelist(&graph, file, 0, false);                    \
+        fclose(file);                                                          \
+                                                                               \
+        while ((tries < 3) || (global_elapsed < 60))                           \
+        {                                                                      \
+            struct timespec start;                                             \
+            clock_gettime(CLOCK_REALTIME, &start);                             \
+                                                                               \
+            igraph_integer_t diameter = function(&graph);                      \
+                                                                               \
+            struct timespec end;                                               \
+            clock_gettime(CLOCK_REALTIME, &end);                               \
+                                                                               \
+            total_diameter += diameter;                                        \
+                                                                               \
+            double start_double = start.tv_nsec * 1E-9 + start.tv_sec;         \
+            double end_double = end.tv_nsec * 1E-9 + end.tv_sec;               \
+                                                                               \
+            total_elapsed += end_double - start_double;                        \
+                                                                               \
+            struct timespec current_time;                                      \
+            clock_gettime(CLOCK_REALTIME, &current_time);                      \
+            double current_time_double =                                       \
+                current_time.tv_nsec * 1E-9 + current_time.tv_sec;             \
+                                                                               \
+            global_elapsed = current_time_double - global_start_double;        \
+                                                                               \
+            tries += 1;                                                        \
+        }                                                                      \
+                                                                               \
+        igraph_destroy(&graph);                                                \
+                                                                               \
+        printf("Function: %s\n", #function);                                   \
+        printf("Tries: %d\n", tries);                                          \
+        printf("Total elapsed: %f s\n", global_elapsed);                       \
+        printf("Total elapsed (without file loading): %f s\n", total_elapsed); \
+        printf("Loading time: %f s\n", global_elapsed - total_elapsed);        \
+        printf("Average time per try: %f s\n", total_elapsed / tries);         \
+        printf("Average try per second: %f try/s\n", tries / total_elapsed);   \
+        printf("Average diameter: %f\n", total_diameter / tries);              \
+        printf("\n");                                                          \
     } while(false)
