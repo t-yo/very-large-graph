97db2fe569ce1c3bacd458d82fa14a22cfa1882e
Axen Georget <mail@axen.io>
2020-05-28 11:02:04 +0200

    feat: benchmark: move file loading in the loop again

--- src/benchmark.h
+++ src/benchmark.h
@@ -14,27 +14,29 @@
         init_stopwatch(&total_elapsed);                                      \
         double total_diameter = 0;                                           \
                                                                              \
-        printf("--------------------------------------------------\n");      \
-        printf("FUNCTION: %s\n", #function);                                 \
+        printf("--------------------------------------------------\n");    \
+        printf("FUNCTION: %s\n\n", #function);                                 \
                                                                              \
         stopwatch_point_t global_start;                                      \
         create_stopwatch_point(&global_start);                               \
                                                                              \
         int tries = 0;                                                       \
                                                                              \
-        fprintf(stderr, "\nStart file reading\n");                             \
-                                                                             \
-        FILE* file = fopen((path), "r");                                     \
-        igraph_t graph;                                                      \
-        igraph_read_graph_edgelist(&graph, file, 0, false);                  \
-        fclose(file);                                                        \
-                                                                             \
         while ((tries < min_tries) || (global_elapsed.real_time < min_time)) \
         {                                                                    \
+            if (tries < min_tries)                                           \
+            {                                                                \
+                fprintf(stderr, "Read file ... ");                         \
+            }                                                                \
+                                                                             \
+            FILE* file = fopen((path), "r");                                 \
+            igraph_t graph;                                                  \
+            igraph_read_graph_edgelist(&graph, file, 0, false);              \
+            fclose(file);                                                    \
                                                                              \
             if (tries < min_tries)                                           \
             {                                                                \
-                fprintf(stderr, "Start try number %d ... ", tries);          \
+                fprintf(stderr, "start try number %d ... ", tries);          \
             }                                                                \
                                                                              \
             stopwatch_point_t start_point;                                   \
@@ -53,20 +55,20 @@
             total_diameter += diameter;                                      \
             increment_stopwatch(&start_point, &end_point, &total_elapsed);   \
                                                                              \
+            igraph_destroy(&graph);                                          \
+                                                                             \
+            tries += 1;                                                      \
+                                                                             \
             stopwatch_point_t current_time;                                  \
             create_stopwatch_point(&current_time);                           \
             create_stopwatch(&global_start, &current_time, &global_elapsed); \
-                                                                             \
-            tries += 1;                                                      \
         }                                                                    \
-                                                                             \
-        igraph_destroy(&graph);                                              \
                                                                              \
         stopwatch_t result;                                                  \
                                                                              \
         printf("\n- Tries:\t\t\t\t%d\n", tries);                             \
                                                                              \
-        printf("- Total elapsed:\t\t\t");                                  \
+        printf("- Total elapsed:\t\t\t");                                    \
         print_stopwatch(&global_elapsed);                                    \
                                                                              \
         printf("\n- Total elapsed (without file loading):\t");               \
@@ -79,7 +81,7 @@
         multiply_scalar_stopwatch(&total_elapsed, 1.0 / tries, &result);     \
         print_stopwatch(&result);                                            \
                                                                              \
-        printf("\n- Average try per second:\t\t%f try/s\n",                \
+        printf("\n- Average try per second:\t\t%f try/s\n",                  \
             tries / total_elapsed.real_time);                                \
         printf("- Average diameter:\t\t\t%f\n\n\n", total_diameter / tries); \
     } while(false)
